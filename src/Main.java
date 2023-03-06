c1162f3b90cfa49270b7a4e0fd84bf11481f724c
Aleksandr <hitrovaleks2014@gmail.com>
2023-03-06 23:03:02 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+            int price = 1000;
+            int bonusPrice = 20;
+            int mile;
+            mile = price / bonusPrice;
+            System.out.println("Ваш бонус " + mile + " миль");
+        }
+    }
